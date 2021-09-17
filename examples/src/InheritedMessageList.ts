9473decb89bb59f22ed6dd6e8a96c909ff34b37b
Nonoroazoro <xiaochao.k@gmail.com>
2021-09-17 11:17:13 +0800

    Override renderListEmpty

--- examples/src/InheritedMessageList.ts
+++ examples/src/InheritedMessageList.ts
@@ -27,6 +27,18 @@ export class InheritedMessageList extends AbstractVirtualizedList<Message>
         return item;
     }
 
+    // protected override renderListEmpty()
+    // {
+    //     const empty = this.createElement("div");
+    //     empty.textContent = "No Items";
+    //     empty.style.textAlign = "center";
+    //     empty.style.padding = "100px";
+    //     empty.style.border = "1px solid green";
+    //     empty.style.margin = "20px";
+    //     // empty.style.marginBottom = "500px";
+    //     return empty;
+    // }
+
     public generateItemKey(itemData: Message)
     {
         return `key${itemData.id}`;
