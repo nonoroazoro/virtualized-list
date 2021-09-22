c90f88d8ff458ba2c3d974b8e425b14f675aa030
Nonoroazoro <xiaochao.k@gmail.com>
2021-09-22 10:58:37 +0800

    Add support for List Header, Footer and Empty placeholder

--- src/components/VirtualizedList/VirtualizedList.ts
+++ src/components/VirtualizedList/VirtualizedList.ts
@@ -1,41 +1,61 @@
 import { AbstractVirtualizedList } from "../AbstractVirtualizedList";
-import type { ItemKeyGenerator, ItemRenderer } from "../../types";
 import type { VirtualizedListOptions } from "../AbstractVirtualizedList";
+import type { VirtualizedListRenderers } from "./VirtualizedListRenderers";
 
 /**
  * Virtualized list component.
  */
 export class VirtualizedList<DataType> extends AbstractVirtualizedList<DataType>
 {
-    private _generateItemKey: ItemKeyGenerator<DataType>;
-    private _renderListItem: ItemRenderer<DataType>;
+    private _renderers: VirtualizedListRenderers<DataType>;
 
     /**
      * Creates an instance of {@link VirtualizedList}.
      *
-     * @param {(VirtualizedListOptions & {
-     *         renderListItem: ItemRenderer<DataType>;
-     *         generateItemKey: ItemKeyGenerator<DataType>;
-     *     })} options The options for the virtualized list.
+     * @param {(VirtualizedListOptions & VirtualizedListRenderers<DataType>)} options The options for the virtualized list.
      */
-    constructor(options: VirtualizedListOptions & {
-        generateItemKey: ItemKeyGenerator<DataType>;
-        renderListItem: ItemRenderer<DataType>;
-    })
+    constructor(options: VirtualizedListOptions & VirtualizedListRenderers<DataType>)
     {
-        const { generateItemKey, renderListItem, ...restOptions } = options;
+        const {
+            generateItemKey,
+            renderListItem,
+            renderListEmpty,
+            renderListHeader,
+            renderListFooter,
+            ...restOptions
+        } = options;
         super(restOptions);
-        this._generateItemKey = generateItemKey;
-        this._renderListItem = renderListItem;
+        this._renderers = {
+            generateItemKey,
+            renderListItem,
+            renderListEmpty,
+            renderListHeader,
+            renderListFooter
+        };
     }
 
     protected renderListItem(itemData: DataType, key: string, index: number)
     {
-        return this._renderListItem(itemData, key, index);
+        return this._renderers.renderListItem.call(this, itemData, key, index);
     }
 
     public generateItemKey(itemData: DataType, index: number)
     {
-        return this._generateItemKey(itemData, index);
+        return this._renderers.generateItemKey.call(this, itemData, index);
+    }
+
+    public override renderListEmpty()
+    {
+        return this._renderers.renderListEmpty?.call(this);
+    }
+
+    public override renderListHeader()
+    {
+        return this._renderers.renderListHeader?.call(this);
+    }
+
+    public override renderListFooter()
+    {
+        return this._renderers.renderListFooter?.call(this);
     }
 }
