4b4072d3bc37641e5448a468b1f7e64073cd81c9
Nonoroazoro <xiaochao.k@gmail.com>
2021-09-22 10:57:29 +0800

    Add HTMLElementRenderer

---
+++ src/types/HTMLElementRenderer.ts
@@ -0,0 +1,6 @@
+/**
+ * Renders an {@link HTMLElement}.
+ *
+ * @returns Returns the {@link HTMLElement}.
+ */
+export type HTMLElementRenderer = () => HTMLElement | undefined;
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,6 @@
 export * from "./dimensions";
 
+export * from "./HTMLElementRenderer";
 export * from "./ItemData";
 export * from "./ItemDataset";
 export * from "./ItemKeyGenerator";
